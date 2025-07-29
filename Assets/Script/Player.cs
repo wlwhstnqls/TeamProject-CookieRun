e528a4bbd0c362fe35bbe5c4d6669e875166f4a6
wlwhstnqls <wlwhstnqls5@gmail.com>
2025-07-29 14:41:24 +0900

    Update Player.cs

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Animator animator = null;
+    Rigidbody2D rb = null;
+
+    public float speed = 5f;
+    public bool isDead = false;
+    public float jumpForce = 5f;
+    public bool isJumping = false;
     void Start()
     {
-        
+      animator =transform.GetComponent<Animator>();
+        rb = transform.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            isJumping = true;
+        }
+
+    }
+
+    public void FixedUpdate()
+    {
+      Vector2 velocity = rb.velocity;
+      velocity.x = speed;
+        if (isJumping)
+        {
+            velocity.y += jumpForce;
+            isJumping = false;
+        }
+        rb.velocity = velocity;
+
+        float angle =Mathf.Clamp((rb.velocity.y * 10f), -90, 90);
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
     }
 }
