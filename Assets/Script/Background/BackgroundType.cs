c66bdd2cad5a47f9da29b59e1858beea0d0f18d3
NakHyeong <snh0719@gmail.com>
2025-07-30 14:09:56 +0900

    테스트
    무한모드가 있다면? 없다면 삭제

---
+++ Assets/Script/Background/BackgroundType.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BackgroundType
+{
+    Tree,       // 초원
+    Desert,     // 사막
+    Morning     // 새벽
+}
