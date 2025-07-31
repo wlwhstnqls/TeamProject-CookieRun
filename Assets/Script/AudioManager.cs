f01eee79e48b6338df51a97a598781468b314be6
wlwhstnqls <wlwhstnqls5@gmail.com>
2025-07-31 10:10:30 +0900

    오디오 배열로관리

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -5,42 +5,30 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     static AudioManager audioManager;
-    
+    public AudioClip[] audioClip;
+    private AudioSource audioSource;
+
     public static AudioManager Instance
     {
         get { return audioManager; }
     }
+   
 
     private void Awake()
     {
         audioManager = this;
-    }
-    void Start()
-    {
-        GetComponent<AudioSource>().mute = true;
-        GetComponents<AudioSource>()[1].mute = true; 
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
+  
 
+    
+   
     public void JumpSound(int idx)
     {
-        
-        if (idx == 0)
-        {
-            GetComponent<AudioSource>().mute = false;
-            AudioSource audioSource = GetComponent<AudioSource>();
-            audioSource.PlayOneShot(audioSource.clip); // 첫번째 점프 사운드 재생
-        }
-        else if (idx == 1)
-        {
-            GetComponents<AudioSource>()[1].mute = false;
-            AudioSource audioSource = GetComponents<AudioSource>()[1];
-            audioSource.PlayOneShot(audioSource.clip); // 두번째 점프 사운드 재생
-        }
+
+        audioSource.PlayOneShot(audioClip[idx]);
+
+
     }
 }
