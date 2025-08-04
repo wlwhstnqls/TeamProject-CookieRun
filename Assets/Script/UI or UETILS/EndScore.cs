cf07ade49bd84a40792780123c5f24f974066da5
wlwhstnqls <wlwhstnqls5@gmail.com>
2025-08-04 16:19:39 +0900

    정상작동

--- Assets/Script/UI or UETILS/EndScore.cs
+++ Assets/Script/UI or UETILS/EndScore.cs
@@ -16,22 +16,7 @@ public class EndScore : MonoBehaviour
         int LoadedScore = PlayerPrefs.GetInt("Score", 0);
         scoreText.text = LoadedScore.ToString();
 
-        ShowAchievements();
-
-        // 업적 불러오기
-        void ShowAchievements()
-        {
-            List<string> achievements = Achievement.Instance.GetAchievementsThisGame();
-
-            if (achievements.Count == 0)
-            {
-                achievementListText.text = "획득한 업적이 없습니다.";
-            }
-            else
-            {
-                achievementListText.text = string.Join("\n", achievements);
-            }
-        }
+       
     }
 
     // Update is called once per frame
